f6708060a81b822ddbd8306269b1549d4acf0bce
MiriamJB <mjb376@ensign.edu>
2023-04-03 17:52:24 -0600

    fix: display tailwind correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,css,js}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                serif: ['Cormorant', 'serif'],
-                mono: ['Fira Code', 'mono'],
-                cursive: ['Itim', 'cursive'],
-            },
-        },
+  content: ["index.html", "app.css", "app.js", "components.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Cormorant", "serif"],
+        mono: ["Fira Code", "mono"],
+        cursive: ["Itim", "cursive"],
+      },
     },
-    plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
